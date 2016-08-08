15ba07859a56c17dcf4f4266b41b6b0db48fca9f
blandry <ben.acc.landry@gmail.com>
2016-08-07 18:43:50 -0700

    iofirmware: switching to json messaging

--- iofirmware/iofirmware.ino
+++ iofirmware/iofirmware.ino
@@ -1,45 +1,48 @@
 #include "config.h"
-#include "CmdMessenger.h"
 #include "Adafruit_PWMServoDriver.h"
+#include <ArduinoJson.h>
 
-enum {
-  echo,
-  servo,
-};
 
 // 1000000 / 60 / 4096
 double pulselength = 4.06901041667;
 
 Adafruit_PWMServoDriver pwm_out = Adafruit_PWMServoDriver();
-CmdMessenger c = CmdMessenger(Serial,',',';','/');
+StaticJsonBuffer<200> json_buffer;
+String serial_buffer;
 
-void on_echo(void){
-
-  int value = c.readBinArg<int>();
-  c.sendBinCmd(echo,value);
-  
+void on_servo(JsonObject& msg){
+    
+    int id = msg["id"];
+    int value = msg["value"];
+    
+    pwm_out.setPWM(id, 0, ((double)value)/pulselength);
 }
 
-void on_servo(void){
-
-  unsigned int id = c.readBinArg<unsigned int>();
-  unsigned int value = c.readBinArg<unsigned int>();
-
-  pwm_out.setPWM(id, 0, ((double)value)/pulselength);
-}
-
-void attach_callbacks(void){
-  c.attach(echo, on_echo);
-  c.attach(servo, on_servo);
+void on_unknown(JsonObject& msg){
+    
+    // todo
 }
 
 void setup() {
-  Serial.begin(COMPANION_BAUD);
-  pwm_out.begin();
-  pwm_out.setPWMFreq(60);
-  attach_callbacks();
+    
+    Serial.begin(COMPANION_BAUD);
+    pwm_out.begin();
+    pwm_out.setPWMFreq(60);
 }
 
 void loop() {
-  c.feedinSerialData();
+    
+    while(Serial.available()) {
+        serial_buffer = Serial.readString();
+        JsonObject& msg = json_buffer.parseObject(serial_buffer);
+        int cmd_id = msg["cmd"];
+        
+        switch (cmd_id) {
+            case 1:
+                on_servo(msg);
+                break;
+            default:
+                on_unknown(msg);
+        }
+    }
 }
